127d7353c2130d4fdc3daa9d0ec84a48e95bdaea
Yunus <21yunuscelik21@gmail.com>
2024-07-30 13:26:47 +0300

    use Link tag in Navbar.js routes

--- client/src/comp/Navbar.js
+++ client/src/comp/Navbar.js
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>My Website</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/about">About</a>
-                <a href="/login" id="loginbutton">Log in</a>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/login" id="loginbutton">Log in</Link>
             </div>
         </nav>
     );
