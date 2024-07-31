231eb36218c40e2a7af00e2e124e65500d1d7b8b
Yunus <21yunuscelik21@gmail.com>
2024-07-31 19:43:06 +0300

    add note details page

--- client/src/comp/NoteDetails.js
+++ client/src/comp/NoteDetails.js
@@ -1,11 +1,16 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const NoteDetails = () => {
     const {id} = useParams();
+    const {data: notes, isLoading, error} = useFetch("http://localhost:8000/notes/" + id);
 
      return (
         <div>
             <h1>Note Details - {id}</h1>
+            <h4>{isLoading && <div>Loading...</div>}</h4>
+            <h4>{error && <div>{error}</div>}</h4>
+            <h4>{notes && <div>{notes.text}</div>}</h4>
         </div>
      );
 }
