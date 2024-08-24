a1085961ebb939bbd65ce3d7270833e812002c4e
Yunus <21yunuscelik21@gmail.com>
2024-08-24 22:33:53 +0300

    add create table query

--- server/src/queries.js
+++ server/src/queries.js
@@ -1,8 +1,15 @@
-const getNotes = "SELECT * FROM notes";
-const getNote = "SELECT * FROM notes WHERE id = $1";
+const table = "notes";
+const createTable = `CREATE TABLE ${table} (title varchar(50),
+                    text varchar(2000),
+                    id SERIAL PRIMARY KEY, 
+                    postdate date DEFAULT now());`;
 
-const postNote = "INSERT INTO notes VALUES ($1, $2)";
-const deleteNote = "DELETE FROM notes WHERE id = $1";
-const updateNote = "UPDATE notes SET title = $1, text = $2 WHERE id = $3;";
 
-module.exports = {getNotes, getNote, postNote, deleteNote, updateNote};
+const getNotes = `SELECT * FROM ${table}`;
+const getNote = `SELECT * FROM ${table} WHERE id = $1`;
+
+const postNote = `INSERT INTO ${table} VALUES ($1, $2)`;
+const deleteNote = `DELETE FROM ${table} WHERE id = $1`;
+const updateNote = `UPDATE ${table} SET title = $1, text = $2 WHERE id = $3;`;
+
+module.exports = {table, createTable, getNotes, getNote, postNote, deleteNote, updateNote};
